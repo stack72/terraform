3cbe9a9f9e3679dd4c44dfb7912ee5c0c9e99bfb
stack72 <public@paulstack.co.uk>
2015-09-17 19:25:56 +0100

    Toying with a new way of playing out the docs

--- website/helpers/sidebar_helpers.rb
+++ website/helpers/sidebar_helpers.rb
@@ -2,11 +2,14 @@ module SidebarHelpers
   # This helps by setting the "active" class for sidebar nav elements
   # if the YAML frontmatter matches the expected value.
   def sidebar_current(expected)
-    current = current_page.data.sidebar_current || ""
-    if current == expected or (expected.is_a?(Regexp) and expected.match(current))
-      return " class=\"active\""
-    else
-      return ""
+    current = current_page.data.sidebar_current || []
+
+    current.each do |active|
+      if active == expected or (expected.is_a?(Regexp) and expected.match(active))
+        return " class=\"active\""
+      else
+        return ""
+      end
     end
   end
 end
